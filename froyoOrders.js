44b6c29ac9961e0c6ded47fb7dcf16b74590c281
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-09-21 17:23:21 -0500

    froyo .js with some nonsense

--- froyoOrders.js
+++ froyoOrders.js
@@ -6,23 +6,32 @@ const userInputFlavors = prompt(
 );
 
 // The user's input is split into an array of strings.
-
-const flavorsArray = userInputFlavors.split(",");
-console.log(flavorsArray);
+const flavors = userInputFlavors.split(",");
 
 // A loop is used to iterate through the array
-
-for( let i=0; i < flavorsArray.length; i++){
-    console.log(flavorsArray[i])
+for( let i=0; i < flavors.length; i++){
+    console.log(flavors[i])
 };
 
 //An object is used to keep count of how many orders of each flavor.
+const froyoOrder = {
+    vanilla: 0,
+    chocolate: 0,
+    coffee: 0,
+    strawberry: 0,
+    mint: 0,
+};
+
+for (const flavor of flavors[str]){
+    
+};
+console.table(froyoOrder);
 
 
 
-
-
-
+for (const flavor of froyoOrder){
+    console.log ('${flavor}: ${froyoOrder[flavor]}'); 
+}
 
 //program correctly counts the number of each flavor
 
@@ -34,3 +43,33 @@ for( let i=0; i < flavorsArray.length; i++){
 //console output changes depending on users input
 
 //
+
+
+
+// const yogurt = {
+//    vanilla: 0,
+//    chocolate: 0,
+//    strawberry: 0,
+//    coffee: 0,
+//  };
+  
+  // Count for each flavor
+  // vanillaCount = 0;
+  // chocolateCount = 0;
+  // strawberryCount = 0;
+  // coffeeCount = 0;
+  
+//  const userInput = prompt (
+//    'Please list each flavor of yogurt to order (seperated by a comma)'
+//  );
+  
+//  const flavors = userInput.split (',');
+  //  create an array of flavor choices based on userInput
+  
+//  for (const flavor of flavors) {
+//    if (yogurt.hasOwnProperty (flavor.toLowerCase ())) yogurt[flavor]++;
+//  }
+  
+//  for (const flavor in yogurt) {
+//    console.log (`${flavor}: ${yogurt[flavor]}`);
+//  }
